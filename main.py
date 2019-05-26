431159d57173d6c8e8fef7b8fcf675a2deb7e21e
Joona Savela <joona.savela@aalto.fi>
2019-05-26 11:58:44 +0300

    add more utility functions

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import matplotlib.pyplot as plt
-from utils import get_data
+from utils import get_data, normalize_prices, times_min_max, is_valid_interval
 
 def main():
     url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=CV8VMYTJCQBBSKXP&outputsize=full"
     times, opens, highs, lows, closes, volumes = get_data(url)
+    opens, highs, lows, closes = normalize_prices(opens, highs, lows, closes)
+
+    time_min, time_max = times_min_max(times)
+
+    # print(is_valid_interval(times[0], 0, 5, time_min, time_max, 1))
+    # print(is_valid_interval(times[0], 0, 5, time_min, time_max, 5))
+    # print(is_valid_interval(times[0], 0, 10000, time_min, time_max, 5))
+    # print(is_valid_interval(times[100], 100, 100, time_min, time_max))
+    # print(is_valid_interval(times[0], 1, 100, time_min, time_max))
 
     # print(len(times))
-    plt.plot(times, closes / opens[0])
-    plt.show()
+    # plt.plot(times, closes)
+    # plt.show()
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def get_data(url):
     # get the data in json format
@@ -32,3 +32,20 @@ def get_data(url):
         volumes = np.append(volumes, float(item["5. volume"]))
 
     return times, opens, highs, lows, closes, volumes
+
+def normalize_prices(opens, highs, lows, closes):
+    divider = opens[0]
+    return opens / divider, highs / divider, lows / divider, closes / divider
+
+def is_same_day(datetime1, datetime2):
+    return datetime1.date() == datetime2.date()
+
+def times_min_max(times):
+    times_without_dates = list(map(lambda x: x.time(), list(times)))
+    return min(times_without_dates), max(times_without_dates)
+
+def is_valid_interval(dt, left, right, time_min, time_max, minutes = 1):
+    datetime1 = dt - timedelta(minutes = left * minutes)
+    datetime2 = dt + timedelta(minutes = right * minutes)
+    return is_same_day(dt, datetime1) and is_same_day(dt, datetime2) and \
+        datetime1.time() >= time_min and datetime2.time() <= time_max
