f0aa82d47b4bc7cf5ac4d292e65e62731b9f0a9c
Joona Savela <joona.savela@aalto.fi>
2019-05-26 11:13:12 +0300

    small changes

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 from utils import get_data
 
 def main():
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=CV8VMYTJCQBBSKXP"
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=CV8VMYTJCQBBSKXP&outputsize=full"
     times, opens, highs, lows, closes, volumes = get_data(url)
 
-    # print(times)
-    plt.plot(times, closes / opens[-1])
+    # print(len(times))
+    plt.plot(times, closes / opens[0])
     plt.show()
 
 if __name__ == "__main__":
