5e1d6339c462025016370094de4cc7dacbcecbfb
Joona Savela <joona.savela@aalto.fi>
2019-06-11 21:41:44 +0300

    update model output layer

--- model.py
+++ model.py
@@ -2,8 +2,20 @@ import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.models import Model, Input, Sequential
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten, concatenate
 from keras.optimizers import Adam
+
+def build_model_old(input_length = 4 * 14, alpha = 0.001):
+    input_layer = Input(shape=(input_length,6)) # 6 fields in json
+    flatten = Flatten()(input_layer)
+    layer_1 = Dense(input_length * 3)(flatten)
+    layer_2 = Dense(input_length * 3 // 2)(layer_1)
+    layer_3 = Dense(input_length * 3 // 4)(layer_2)
+    output_layer = Dense(4, activation='softmax')(layer_3)
+    model = Model(input_layer, output_layer)
+    model.compile(Adam(), 'mse', metrics=['accuracy'])
+
+    return model
 
 def build_model(input_length = 4 * 14, alpha = 0.001):
     input_layer = Input(shape=(input_length,6)) # 6 fields in json
@@ -11,7 +23,9 @@ def build_model(input_length = 4 * 14, alpha = 0.001):
     layer_1 = Dense(input_length * 3)(flatten)
     layer_2 = Dense(input_length * 3 // 2)(layer_1)
     layer_3 = Dense(input_length * 3 // 4)(layer_2)
-    output_layer = Dense(4, activation='softmax')(layer_3)
+    output_layer1 = Dense(3, activation='softmax')(layer_3)
+    output_layer2 = Dense(1, activation='sigmoid')(layer_3)
+    output_layer = concatenate([output_layer1, output_layer2])
     model = Model(input_layer, output_layer)
     model.compile(Adam(), 'mse', metrics=['accuracy'])
 
