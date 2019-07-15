3da655631dd8c084214e4f89a051727d44fff812
Joona Savela <joona.savela@aalto.fi>
2019-07-15 08:12:47 +0300

    fix another bug with deque criterion

--- trade.py
+++ trade.py
@@ -101,7 +101,7 @@ def trading_pipeline():
         take_profit = Take_profit_criterion(0.01)
         trend_criterion = Trend_criterion(0.02)
         deque_criterion = Deque_criterion(3, 10 * 60)
-        # deque_criterion.sell_time = initial_time / 60
+        deque_criterion.sell_time = initial_time / 60 - deque_criterion.waiting_time - 1
 
         wealths = get_trades(count = 4)
         trades = wealths[-3:] / wealths[:3]
@@ -144,7 +144,7 @@ def trading_pipeline():
                         trend_criterion.buy(ma_corrected[-1])):
                     take_profit.buy_price = price
                     stop_loss.buy_price = price
-                    deque_criterion.sell_time = None
+                    # deque_criterion.sell_time = None
                     balance_usdt = asset_balance(client, 'USDT')
                     high = binance_price(client, symbol)
                     # cancel_orders(client, symbol, 'SELL')
