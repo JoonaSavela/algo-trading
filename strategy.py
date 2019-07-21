a257b8000a6b736a36a500becdc83230be9edf2f
Joona Savela <joona.savela@aalto.fi>
2019-07-21 11:38:18 +0300

    make strategies more reusable and modifiable

--- strategy.py
+++ strategy.py
@@ -15,30 +15,36 @@ class Stochastic_criterion:
         self.buy_threshold = buy_threshold
         self.sell_threshold = sell_threshold if sell_threshold is not None else buy_threshold
 
-    def buy(self, stoch):
+    def buy(self, args):
+        stoch = args['stoch']
         return stoch < self.buy_threshold
 
-    def sell(self, stoch):
+    def sell(self, args):
+        stoch = args['stoch']
         return stoch > 1 - self.sell_threshold
 
 class Heikin_ashi_criterion:
     def __init__(self):
         pass
 
-    def buy(self, ha):
+    def buy(self, args):
+        ha = args['ha']
         return ha[1] == ha[3]
 
-    def sell(self, ha):
+    def sell(self, args):
+        ha = args['ha']
         return ha[1] == ha[2]
 
 class Trend_criterion:
     def __init__(self, threshold):
         self.threshold = threshold
 
-    def buy(self, ma_corrected):
+    def buy(self, args):
+        ma_corrected = args['ma_corrected']
         return ma_corrected > self.threshold
 
-    def sell(self, ma_corrected):
+    def sell(self, args):
+        ma_corrected = args['ma_corrected']
         return ma_corrected < -self.threshold
 
 class Stop_loss_criterion:
@@ -46,10 +52,11 @@ class Stop_loss_criterion:
         self.stop_loss = stop_loss
         self.buy_price = None
 
-    def buy(self):
+    def buy(self, args):
         return self.buy_price is None
 
-    def sell(self, close):
+    def sell(self, args):
+        close = args['close']
         return self.buy_price is not None and close / self.buy_price - 1 < self.stop_loss
 
 
@@ -58,10 +65,11 @@ class Take_profit_criterion:
         self.take_profit = take_profit
         self.buy_price = None
 
-    def buy(self):
+    def buy(self, args):
         return self.buy_price is None
 
-    def sell(self, close):
+    def sell(self, args):
+        close = args['close']
         return self.buy_price is not None and close / self.buy_price - 1 > self.take_profit
 
 
@@ -70,8 +78,6 @@ class Deque_criterion:
         self.trades = deque(maxlen = maxlen)
         self.waiting_time = waiting_time
         self.sell_time = - self.waiting_time
-        self.recently_bought = False
-        self.recently_sold = False
 
     def get_profit(self):
         res = 1.0
@@ -85,11 +91,12 @@ class Deque_criterion:
     def get_waiting_time(self, profit):
         return self.waiting_time * (- profit * 100)
 
-    def buy(self, current_time):
+    def buy(self, args):
+        current_time = args['current_time']
         profit = self.get_profit() - 1.0
         return profit >= 0.0 or current_time - self.sell_time > self.get_waiting_time(profit)
 
-    def sell(self):
+    def sell(self, args):
         return True
 
 
@@ -98,136 +105,274 @@ class Logic_criterion:
         self.recently_bought = False
         self.recently_sold = False
 
-    def buy(self):
+    def buy(self, args):
         return not self.recently_bought
 
-    def sell(self):
+    def sell(self, args):
         return self.recently_bought and not self.recently_sold
 
 
-def evaluate_strategy(files):
-    window_size1 = 3 * 14
-    window_size2 = 1 * 14
-    window_size3 = 1 * 14
-    k = 1
-    latency = 0
+class Base_Strategy:
+    def __init__(self, params, stop_loss_take_profit, restrictive):
+        self.stop_loss = Stop_loss_criterion(params['stop_loss'])
+        self.take_profit = Take_profit_criterion(params['take_profit'])
+        self.deque_criterion = Deque_criterion(params['maxlen'], params['waiting_time'])
+        self.logic_criterion = Logic_criterion()
+
+        self.buy_and_criteria = []
+
+        self.sell_and_criteria = []
+
+        self.buy_or_criteria = []
+
+        self.sell_or_criteria = []
+
+        if stop_loss_take_profit:
+            self.buy_and_criteria.extend([
+                self.stop_loss,
+                self.take_profit,
+            ])
+            self.sell_or_criteria.extend([
+                self.stop_loss,
+                self.take_profit,
+            ])
+
+        if restrictive:
+            self.buy_and_criteria.extend([
+                self.deque_criterion,
+                self.logic_criterion
+            ])
+            self.sell_and_criteria.extend([
+                self.logic_criterion,
+                self.deque_criterion
+            ])
+
+
+    def get_buy_and_rules(self, args):
+        return all(map(lambda x: x.buy(args), self.buy_and_criteria))
+
+    def get_sell_and_rules(self, args):
+        return all(map(lambda x: x.sell(args), self.sell_and_criteria))
+
+    def get_buy_or_rules(self, args):
+        return any(map(lambda x: x.buy(args), self.buy_or_criteria))
+
+    def get_sell_or_rules(self, args):
+        return any(map(lambda x: x.sell(args), self.sell_or_criteria))
+
+    def buy(self, args):
+        return self.get_buy_and_rules(args) and self.get_buy_or_rules(args)
+
+    def sell(self, args):
+        return self.get_sell_and_rules(args) and self.get_sell_or_rules(args)
+
+    def update_after_buy(self, price):
+        self.take_profit.buy_price = price
+        self.stop_loss.buy_price = price
+        self.logic_criterion.recently_bought = True
+        self.logic_criterion.recently_sold = False
+
+    def update_after_sell(self, price, i):
+        if self.take_profit.buy_price is not None:
+            trade = price / self.take_profit.buy_price
+            self.deque_criterion.append(trade)
+        self.take_profit.buy_price = None
+        self.stop_loss.buy_price = None
+        self.deque_criterion.sell_time = i
+        self.logic_criterion.recently_bought = False
+        self.logic_criterion.recently_sold = True
+
+    def reset(self):
+        self.take_profit.buy_price = None
+        self.stop_loss.buy_price = None
+        self.deque_criterion.sell_time = -1
+        self.deque_criterion.trades.clear()
+        self.logic_criterion.recently_bought = False
+        self.logic_criterion.recently_sold = False
+
+
+class Simple_Strategy(Base_Strategy):
+    def __init__(self, params, stop_loss_take_profit = False, restrictive = False):
+        super().__init__(params, stop_loss_take_profit, restrictive)
+        self.window_size = params['window_size1'] * 14
+
+        self.stochastic_criterion = Stochastic_criterion(params['buy_threshold'], params['sell_threshold'])
+
+        self.buy_or_criteria.append(
+            self.stochastic_criterion
+        )
+
+        self.sell_or_criteria.append(
+            self.stochastic_criterion
+        )
+
+
+    def get_output(self, X, reset = True):
+        stochastic = stochastic_oscillator(X, self.window_size)
+
+        sequence_length = stochastic.shape[0]
+
+        X = X[-sequence_length:, :]
+
+        buys = np.zeros(sequence_length, dtype=bool)
+        sells = np.zeros(sequence_length, dtype=bool)
+
+        for i in range(sequence_length):
+            price = X[i, 0]
+            stoch = stochastic[i]
+
+            args = {
+                'close': price,
+                'stoch': stoch,
+                'current_time': i,
+            }
+
+            if self.buy(args):
+                self.update_after_buy(price)
+                buys[i] = True
+            elif self.sell(args):
+                self.update_after_sell(price, i)
+                sells[i] = True
+
+        if reset:
+            self.reset()
+
+        return buys, sells
+
+
+class Strategy(Base_Strategy):
+    def __init__(self, params, stop_loss_take_profit = False, restrictive = False):
+        super().__init__(params, stop_loss_take_profit, restrictive)
+        self.window_size1 = params['window_size1'] * 14
+        self.window_size2 = params['window_size2'] * 14
+        self.window_size3 = params['window_size3'] * 14
+
+        self.stochastic_criterion = Stochastic_criterion(params['buy_threshold'], params['sell_threshold'])
+        self.ha_criterion = Heikin_ashi_criterion()
+        self.trend_criterion = Trend_criterion(params['trend_threshold'])
+
+        self.buy_and_criteria.extend([
+            self.ha_criterion
+        ])
+
+        self.sell_and_criteria.extend([
+            self.ha_criterion
+        ])
+
+        self.buy_or_criteria.extend([
+            self.stochastic_criterion,
+            self.trend_criterion
+        ])
+
+        self.sell_or_criteria.extend([
+            self.stochastic_criterion,
+            self.trend_criterion
+        ])
+
+
+    def get_output(self, X, reset = True):
+        ha = heikin_ashi(X)
+
+        tp = np.mean(X[:, :3], axis = 1).reshape((X.shape[0], 1))
+        ma = sma(tp, self.window_size1)
+        sd = std(tp, self.window_size1)
+
+        X_corrected = X[-ma.shape[0]:, :4] - np.repeat(ma.reshape((-1, 1)), 4, axis = 1)
+
+        stochastic = stochastic_oscillator(X_corrected, self.window_size2)
+
+        X_corrected /= np.repeat(X[-X_corrected.shape[0]:, 0].reshape((-1, 1)), 4, axis = 1)
+
+        ma_corrected = sma(X_corrected, self.window_size3)
+
+        sequence_length = np.min([X_corrected.shape[0], ma_corrected.shape[0], stochastic.shape[0], ma.shape[0]])
+
+        X = X[-sequence_length:, :]
+        ha = ha[-sequence_length:, :]
+        ma = ma[-sequence_length:]
+        stochastic = stochastic[-sequence_length:]
+        ma_corrected = ma_corrected[-sequence_length:]
+        sd = sd[-sequence_length:]
+
+        m = 2 # TODO: move
+        upper = ma + m * sd
+        lower = ma - m * sd
+        width = (upper - lower) / ma
+        rolling_min_width = pd.Series(width).rolling(self.window_size2).min().dropna().values
+
+        buys = np.zeros(sequence_length, dtype=bool)
+        sells = np.zeros(sequence_length, dtype=bool)
+
+        for i in range(sequence_length):
+            price = X[i, 0]
+            stoch = stochastic[i]
+
+            args = {
+                'close': price,
+                'stoch': stoch,
+                'current_time': i,
+                'ma_corrected': ma_corrected[i],
+                'ha': ha[i, :]
+            }
+
+            if self.buy(args):
+                self.update_after_buy(price)
+                buys[i] = True
+            elif self.sell(args):
+                self.update_after_sell(price, i)
+                sells[i] = True
+
+        if reset:
+            self.reset()
+
+        return buys, sells
+
+
+
+
+def evaluate_strategy(files, strategy):
+    X = load_all_data(files)
+
+    buys, sells = strategy.get_output(X)
+    sequence_length = buys.shape[0]
+    X = X[-sequence_length:, :]
 
     initial_capital = 1000
     commissions = 0.00075
 
-    stochastic_criterion = Stochastic_criterion(0.04, 0.07)
-    ha_criterion = Heikin_ashi_criterion()
-    stop_loss = Stop_loss_criterion(-0.02)
-    take_profit = Take_profit_criterion(0.01)
-    trend_criterion = Trend_criterion(0.02)
-    deque_criterion = Deque_criterion(3, 12 * 60)
-    logic_criterion = Logic_criterion()
-
-    trades = []
-    current_time = 0
-
-    X = load_all_data(files)
-
-    # stochastic = stochastic_oscillator(X, window_size, k)
-    ha = heikin_ashi(X)
-
-    tp = np.mean(X[:, :3], axis = 1).reshape((X.shape[0], 1))
-    ma = sma(tp, window_size1)
-    sd = std(tp, window_size1)
-
-    X_corrected = X[-ma.shape[0]:, :4] - np.repeat(ma.reshape((-1, 1)), 4, axis = 1)
-
-    stochastic = stochastic_oscillator(X_corrected, window_size2, k, latency)
-
-    X_corrected /= np.repeat(X[-X_corrected.shape[0]:, 0].reshape((-1, 1)), 4, axis = 1)
-
-    ma_corrected = sma(X_corrected, window_size3)
-
-    sequence_length = np.min([X_corrected.shape[0], ma_corrected.shape[0], stochastic.shape[0], ma.shape[0]])
-    # print(sequence_length)
-
-    X = X[-sequence_length:, :]
-    ha = ha[-sequence_length:, :]
-    ma = ma[-sequence_length:]
-    stochastic = stochastic[-sequence_length:]
-    ma_corrected = ma_corrected[-sequence_length:]
-    sd = sd[-sequence_length:]
-
-    m = 2 # TODO: move
-    upper = ma + m * sd
-    lower = ma - m * sd
-    width = (upper - lower) / ma
-    rolling_min_width = pd.Series(width).rolling(window_size2).min().dropna().values
-
     capital_usd = initial_capital
-    trade_start_capital = capital_usd
     capital_coin = 0
 
-    wealths = [initial_capital]
-    capital_usds = [capital_usd]
-    capital_coins = [capital_coin]
-    buy_amounts = []
-    sell_amounts = []
+    wealths = []
+    trades = []
+
+    buy_price = None
 
     i = 0
     while i < sequence_length:
         price = X[i, 0]
-        stoch = stochastic[i]
 
-        if ha_criterion.buy(ha[i, :]) and take_profit.buy() and \
-                stop_loss.buy() and deque_criterion.buy(current_time) and \
-                logic_criterion.buy() and \
-                (stochastic_criterion.buy(stoch) or \
-                trend_criterion.buy(ma_corrected[i])):
-            take_profit.buy_price = price
-            stop_loss.buy_price = price
-            logic_criterion.recently_bought = True
-            logic_criterion.recently_sold = False
+        if buys[i]:
+            buy_price = price
 
             amount_coin = capital_usd / price * (1 - commissions)
             capital_coin += amount_coin
             capital_usd = 0
-            buy_amounts.append(amount_coin * price)
-        elif ha_criterion.sell(ha[i, :]) and logic_criterion.sell() and \
-                (stochastic_criterion.sell(stoch) or \
-                trend_criterion.sell(ma_corrected[i]) or \
-                stop_loss.sell(price) or \
-                take_profit.sell(price)):
-            if take_profit.buy_price is not None:
-                trade = price / take_profit.buy_price
+        elif sells[i]:
+            if buy_price is not None:
+                trade = price / buy_price
                 trades.append(trade - 1)
-                deque_criterion.append(trade)
-            take_profit.buy_price = None
-            stop_loss.buy_price = None
-            deque_criterion.sell_time = current_time
-            logic_criterion.recently_bought = False
-            logic_criterion.recently_sold = True
+            buy_price = None
 
             amount_usd = capital_coin * price * (1 - commissions)
             capital_usd += amount_usd
             capital_coin = 0
-            sell_amounts.append(amount_usd)
 
         wealths.append(capital_usd + capital_coin * price)
-        capital_usds.append(capital_usd)
-        capital_coins.append(capital_coin * price)
 
         i += 1
-        current_time += 1
-
-    price = X[-1, 0]
-    amount_usd = capital_coin * price * (1 - commissions)
-    capital_usd += amount_usd
-    sell_amounts.append(amount_usd)
-    capital_coin = 0
-
-    wealths.append(capital_usd)
-    capital_usds.append(capital_usd)
-    capital_coins.append(capital_coin * price)
 
     wealths = np.array(wealths) / wealths[0]
-    capital_usds = np.array(capital_usds) / initial_capital
-    capital_coins = np.array(capital_coins) / initial_capital
 
     closes = X[:,0] / X[0, 0]
 
@@ -243,18 +388,38 @@ def evaluate_strategy(files):
     plt.hist(trades)
     plt.show()
 
+
+
 if __name__ == '__main__':
+    stop_loss_take_profit = True
+    restrictive = True
+
+    params = {
+        'stop_loss': -0.03,
+        'take_profit': 0.01,
+        'maxlen': 3,
+        'waiting_time': 8 * 60,
+        'buy_threshold': 0.04,
+        'sell_threshold': 0.07,
+        'trend_threshold': 0.02,
+        'window_size1': 3,
+        'window_size2': 1,
+        'window_size3': 1,
+    }
+
+    strategy = Strategy(params, stop_loss_take_profit, restrictive)
+
     dir = 'data/ETH/'
     # for dir in glob.glob('data/*/'):
     test_files = glob.glob(dir + '*.json')
     test_files.sort(key = get_time)
     print(dir, len(test_files), round_to_n(len(test_files) * 2001 / (60 * 24)))
-    evaluate_strategy(test_files)
+    evaluate_strategy(test_files, strategy)
     print()
     test_files = test_files[-20:]
     print(dir, len(test_files), round_to_n(len(test_files) * 2001 / (60 * 24)))
-    evaluate_strategy(test_files)
+    evaluate_strategy(test_files, strategy)
     print()
     test_files = test_files[-3:]
     print(dir, len(test_files), round_to_n(len(test_files) * 2001 / (60 * 24)))
-    evaluate_strategy(test_files)
+    evaluate_strategy(test_files, strategy)
