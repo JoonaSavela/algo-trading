e1f7794974ff0362fb7e9412a8b9cb14d81a6d24
Joona Savela <joona.savela@aalto.fi>
2020-11-30 16:54:54 +0200

    improve trailing stop loss calculation

--- optimize.py
+++ optimize.py
@@ -167,34 +167,64 @@ def get_adaptive_wealths(X, buys, sells, aggregate_N, sltp_args, total_balance, 
                 max_price = price
 
             if trigger_name != 'trailing':
-                trigger_prices = np.repeat(trigger_price, exit_i - entry_i - 1)
+                trigger_prices1 = trigger_prices2 = np.repeat(trigger_price, exit_i - entry_i - 1)
             else:
+                # TODO: add random version of this
                 accumulative_max = np.maximum.accumulate(np.concatenate([
                     np.array([max_price]),
                     sub_X[1:, 1] * price
-                ]))[:-1]
-                max_price = accumulative_max[-1]
-                trigger_prices = accumulative_max - (1 - trigger_param) * buy_price
+                ]))
+                max_price = accumulative_max[-2]
+                trigger_prices1 = accumulative_max[:-1] - (1 - trigger_param) * buy_price
+                trigger_prices2 = accumulative_max[1:] - (1 - trigger_param) * buy_price
 
             if trigger_name == 'take_profit':
-                li = sub_X[1:, 1] * price > trigger_prices
+                li1 = li2 = sub_X[1:, 1] * price > trigger_prices1
             else:
-                li = sub_X[1:, 2] * price < trigger_prices
+                li1 = sub_X[1:, 2] * price < trigger_prices1
+                if trigger_name == 'trailing':
+                    li2 = sub_X[1:, 2] * price < trigger_prices2
+                else:
+                    li2 = li1
+
+            cond1 = np.any(li1)
+            cond2 = np.any(li2)
 
             # check if trigger is triggered
-            if not trigger_triggered and np.any(li):
+            if not trigger_triggered and (cond1 or cond2):
                 idx = np.arange(len(sub_X) - 1) + 1
 
-                idx = idx[li]
-                min_i = idx[0]
-                trigger_price = trigger_prices[li][0]
+                if cond1:
+                    idx1 = idx[li1]
+                    min_i = idx1[0]
+                    trigger_price = trigger_prices1[li1][0]
+                    trigger_triggered = True
+                else:
+                    min_i = len(sub_X) - 1
+                    trigger_price = sub_X[-1, 0] * price
 
-                trigger_triggered = True
-                spread = potential_spreads[np.argmin(np.abs(potential_balances - trigger_price / price * old_wealth * total_balance))]
-                commissions_and_spread = (1 - commissions - spread)
+                if trigger_name == 'trailing':
+                    idx2 = idx[li2]
+                    min_i2 = idx2[0]
+
+                    prev_trigger_price = trigger_price
+                    trigger_price = np.min([
+                        trigger_price,
+                        (max(trigger_prices1[li2][0], sub_X[min_i2, 2] * price) + trigger_prices2[li2][0]) / 2
+                    ])
+
+                    if trigger_price != prev_trigger_price:
+                        trigger_triggered = True
+                        min_i = min_i2
 
                 wealths[entry_i:entry_i + min_i] = sub_X[:min_i, 0] * old_wealth
-                wealths[entry_i + min_i:exit_i] = trigger_price / price * old_wealth * commissions_and_spread
+                wealths[entry_i + min_i:exit_i] = trigger_price / price * old_wealth
+
+                if trigger_triggered:
+                    spread = potential_spreads[np.argmin(np.abs(potential_balances - trigger_price / price * old_wealth * total_balance))]
+                    commissions_and_spread = (1 - commissions - spread)
+
+                    wealths[entry_i + min_i:exit_i] *= commissions_and_spread
 
             elif trigger_triggered:
                 wealths[entry_i:exit_i] = old_wealth
@@ -262,7 +292,7 @@ def apply_commissions_and_spreads(wealths, buys, sells, N, commissions, spread, 
     return wealths
 
 
-# TODO: add min_trade_wealths
+# No commissions or spread
 def get_trade_wealths(X, entries, exits, N):
     entries_idx, exits_idx = get_entry_and_exit_idx(entries, exits, N)
 
@@ -282,16 +312,7 @@ def get_trade_wealths(X, entries, exits, N):
     return trade_wealths, min_trade_wealths, max_trade_wealths
 
 
-def append_trade_wealths(wealths, entries, exits, N, trade_wealths, max_trade_wealths):
-    entries_idx, exits_idx = get_entry_and_exit_idx(entries, exits, N)
-
-    for entry_i in entries_idx:
-        larger_exits_idx = exits_idx[exits_idx > entry_i]
-        exit_i = larger_exits_idx[0] + 1 if larger_exits_idx.size > 0 else N
-        sub_wealths = wealths[entry_i:exit_i]
-        trade_wealths.append(sub_wealths[-1] / sub_wealths[0])
-        max_trade_wealths.append(sub_wealths.max() / sub_wealths[0])
-
+# No commissions or spread
 def get_sub_trade_wealths(X, entries, exits, N):
     entries_idx, exits_idx = get_entry_and_exit_idx(entries, exits, N)
 
@@ -313,23 +334,12 @@ def get_sub_trade_wealths(X, entries, exits, N):
 
     return sub_trade_wealths, min_sub_trade_wealths, max_sub_trade_wealths
 
-def append_sub_trade_wealths(X, entries, exits, N, sub_trade_wealths, min_sub_trade_wealths):
-    entries_idx, exits_idx = get_entry_and_exit_idx(entries, exits, N)
 
-    for entry_i in entries_idx:
-        larger_exits_idx = exits_idx[exits_idx > entry_i]
-        exit_i = larger_exits_idx[0] + 1 if larger_exits_idx.size > 0 else N
-        sub_X = X[entry_i:exit_i, :]
-        sub_wealths = sub_X[1:, 0] / sub_X[:-1, 0]
-        min_sub_wealths = sub_X[1:, 2] / sub_X[:-1, 0]
-        sub_trade_wealths.append(sub_wealths)
-        min_sub_trade_wealths.append(min_sub_wealths)
-
-
-def get_take_profit_wealths_from_trades(trade_wealths, max_trade_wealths, take_profit, total_months, commissions, spread, return_total = True, return_as_log = False):
+# No commissions or spread
+def get_take_profit_wealths_from_trades(trade_wealths, max_trade_wealths, take_profit, total_months, return_total = True, return_as_log = False):
     trade_wealths = np.array(trade_wealths)
     max_trade_wealths = np.array(max_trade_wealths)
-    trade_wealths[max_trade_wealths > take_profit] = take_profit * (1 - commissions - spread)
+    trade_wealths[max_trade_wealths > take_profit] = take_profit
 
     if return_total:
         res = np.sum(np.log(trade_wealths)) / total_months
@@ -337,10 +347,11 @@ def get_take_profit_wealths_from_trades(trade_wealths, max_trade_wealths, take_p
 
     return np.log(trade_wealths) if return_as_log else trade_wealths
 
-def get_stop_loss_wealths_from_trades(trade_wealths, min_trade_wealths, stop_loss, total_months, commissions, spread, return_total = True, return_as_log = False):
+# No commissions or spread
+def get_stop_loss_wealths_from_trades(trade_wealths, min_trade_wealths, stop_loss, total_months, return_total = True, return_as_log = False):
     trade_wealths = np.array(trade_wealths)
     min_trade_wealths = np.array(min_trade_wealths)
-    trade_wealths[min_trade_wealths < stop_loss] = stop_loss * (1 - commissions - spread)
+    trade_wealths[min_trade_wealths < stop_loss] = stop_loss
 
     if return_total:
         res = np.sum(np.log(trade_wealths)) / total_months
@@ -348,8 +359,8 @@ def get_stop_loss_wealths_from_trades(trade_wealths, min_trade_wealths, stop_los
 
     return np.log(trade_wealths) if return_as_log else trade_wealths
 
-
-def get_trailing_stop_loss_wealths_from_sub_trades(sub_trade_wealths, min_sub_trade_wealths, max_sub_trade_wealths, trailing_stop_loss, total_months, commissions, spread, return_total = True, return_as_log = False):
+# No commissions or spread
+def get_trailing_stop_loss_wealths_from_sub_trades(sub_trade_wealths, min_sub_trade_wealths, max_sub_trade_wealths, trailing_stop_loss, total_months, return_total = True, return_as_log = False):
     trade_wealths = np.array([
         sub_trade_wealths[i][-1] for i in range(len(sub_trade_wealths))
     ])
@@ -358,15 +369,30 @@ def get_trailing_stop_loss_wealths_from_sub_trades(sub_trade_wealths, min_sub_tr
         # price starts exactly at 1, otherwise would need to multiply with it
         trailing_diff = 1 - trailing_stop_loss
 
-        # assume the worst case for trailing stop loss
+        # TODO: add a random version of this
         accumulative_max = np.concatenate([
             np.array([1.0]),
             np.maximum.accumulate(max_sub_trade_wealths[i])
-        ])[:-1]
-        li = min_sub_trade_wealths[i] < accumulative_max - trailing_diff
+        ])
+        # for each candle/timestep, low occurs before high
+        accumulative_max1 = accumulative_max[:-1]
+        # for each candle/timestep, high occurs before low
+        accumulative_max2 = accumulative_max[1:]
 
-        if np.any(li):
-            trade_wealths[i] = (accumulative_max[li][0] - trailing_diff) * (1 - commissions - spread)
+        li1 = min_sub_trade_wealths[i] < accumulative_max1 - trailing_diff
+        li2 = min_sub_trade_wealths[i] < accumulative_max2 - trailing_diff
+
+        cond1 = np.any(li1)
+        cond2 = np.any(li2)
+
+        # not possible to have cond1 == True and cond2 == False
+        if cond2:
+            # assume the worst case
+            trade_wealths[i] = np.min([
+                accumulative_max1[li1][0] - trailing_diff if cond1 else trade_wealths[i],
+                (max(accumulative_max1[li2][0] - trailing_diff, min_sub_trade_wealths[i][li2][0]) + accumulative_max2[li2][0] - trailing_diff) / 2
+            ])
+
 
     if return_total:
         res = np.sum(np.log(trade_wealths)) / total_months
@@ -398,6 +424,7 @@ def get_stop_loss_wealths_from_sub_trades(sub_trade_wealths, min_sub_trade_wealt
         return_tuple = return_tuple + (N_transactions,)
 
     return return_tuple
+
 
 def transform_wealths(wealths, X, entries, exits, N, take_profit, stop_loss, commissions, spread):
     assert(take_profit == np.Inf or stop_loss == 0)
